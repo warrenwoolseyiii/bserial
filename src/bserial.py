46e617a6a2b7c44e892f34262cde1ec6e8a01d9a
budgettsfrog <budgettsfrog@protonmail.com>
2023-03-24 13:17:47 -0700

    updated readme

--- src/bserial.py
+++ src/bserial.py
@@ -1,52 +1,75 @@
-import argparse
-import serial
-import sys
-import threading
+#!/usr/bin/env python3
 
+"""bserial.py - A simple command line interface for serial communication
+
+This program provides a command line interface for communicating with a serial port device. It takes two command line arguments, the name of the serial port device and the baud rate of the serial connection. It reads all incoming data from the serial port and prints it to the console, and it also allows the user to send data to the serial port by typing it into the console.
+
+Usage:
+  bserial.py [-h] -p PORT -b BAUD
+
+Options:
+  -h, --help            Show this help message and exit
+  -p PORT, --port=PORT  Name of the serial port device
+  -b BAUD, --baud=BAUD  Baud rate of the serial connection
+
+Example:
+  bserial.py -p /dev/ttyUSB0 -b 9600
+
+"""
+
+import argparse
+import threading
+import sys
+from serial import Serial
 
 def read_serial(serial_port):
+    """Read data from the serial port and print it to the console.
+
+    This function reads data from the specified serial port and prints it to the console.
+    It runs in a separate thread so that it does not block the main thread.
+
+    Args:
+        serial_port (Serial): The Serial object representing the serial port to read from.
+
+    """
     while True:
-        try:
-            if serial_port.in_waiting > 0:
-                data = serial_port.read(serial_port.in_waiting)
-                sys.stdout.write(data.decode('ascii'))
-        except serial.SerialException:
-            print('Failed to read data from serial port')
-            sys.exit(1)
-
-
-def read_console(serial_port):
-    while True:
-        try:
-            data = sys.stdin.read(1)
-            if data:
-                serial_port.write(data.encode('ascii'))
-        except serial.SerialException:
-            print('Failed to write data to serial port')
-            sys.exit(1)
-
+        if serial_port.in_waiting > 0:
+            # Read all available data from the serial port and print it to the console
+            data = serial_port.read_all().decode()
+            sys.stdout.write(data)
+            sys.stdout.flush()
 
 def main(args):
+    """Main function for the bserial program.
+
+    This function is the main entry point for the bserial program. It takes command line arguments
+    specifying the serial port device and baud rate, and then opens a connection to the device and
+    starts a separate thread to read data from the serial port. It also allows the user to send data
+    to the serial port by typing it into the console.
+
+    Args:
+        args (Namespace): A Namespace object containing the command line arguments.
+
+    """
     try:
-        serial_port = serial.Serial(args.port, args.baud)
-    except serial.SerialException:
-        print(
-            f'Failed to connect to serial port {args.port} at {args.baud} baud')
-        sys.exit(1)
+        # Open a connection to the specified serial port
+        serial_port = Serial(args.port, args.baud)
+        print(f"Connected to serial port {args.port} at {args.baud} baud")
 
-    t1 = threading.Thread(target=read_serial, args=[serial_port])
-    t2 = threading.Thread(target=read_console, args=[serial_port])
+        # Start a separate thread to read data from the serial port
+        thread = threading.Thread(target=read_serial, args=(serial_port,), daemon=True)
+        thread.start()
 
-    t1.start()
-    t2.start()
-
-    t1.join()
-    t2.join()
-
+        # Read input from the console and send it to the serial port
+        while True:
+            input_data = input()
+            serial_port.write(input_data.encode())
+    except Exception as e:
+        print(f"Failed to connect to serial port {args.port} at {args.baud} baud")
+        print(e)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Read from and write to a serial port')
+    parser = argparse.ArgumentParser(description='Read from and write to a serial port')
     parser.add_argument('-p', '--port', help='Serial port name')
     parser.add_argument('-b', '--baud', type=int, help='Baud rate')
     args = parser.parse_args()
