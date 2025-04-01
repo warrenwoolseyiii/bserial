f8a008371e12770a7d04a66b1a9e0973d1c2e44a
budgettsfrog <budgettsfrog@protonmail.com>
2025-03-31 21:20:55 -0700

    FTR-12: Added a version file, updated setup.py. Generate binaries for all major OS's on push

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from setuptools import setup, find_packages
+from src.version import get_version
 
 setup(
     name='bserial',
-    version='0.1',
+    version=get_version(),
+    author='Warren Woolsey',
+    description='Cross-platform serial port utility',
+    url='https://github.com/warrenwoolseyiii/bserial',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
+    install_requires=[
+        'pyserial',  # Required for serial communication
+        'tkinter',  # Required for GUI (Python 3.x includes tkinter by default)
+        'glob',  # For file pattern matching (if needed)
+    ],
+    python_requires='>=3.7',
     entry_points={
         'console_scripts': [
             'bserial=bserial.bserial:main',
