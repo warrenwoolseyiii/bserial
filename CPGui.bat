0667392324c6f3fd027e7ac4692212c0ad4413fc
budgettsfrog <budgettsfrog@protonmail.com>
2025-09-22 22:27:58 -0700

    Adding an installation step for dependencies

--- CPGui.bat
+++ CPGui.bat
@@ -1,5 +1,8 @@
 @echo off
 setlocal
 set PYTHONPATH=%~dp0src;%PYTHONPATH%
+echo "Installing dependencies..."
+python.exe -m pip install -r requirements.txt
+echo "Launching bserial..."
 python.exe -m bserial.bserial
 endlocal
